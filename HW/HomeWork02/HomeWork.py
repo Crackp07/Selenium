d428b16ef52442db60b874abb46234be607ced1b
Crackp07 <byushuk@gmail.com>
2023-11-26 18:14:50 +0200

    Selenium

--- HW/HomeWork02/HomeWork.py
+++ HW/HomeWork02/HomeWork.py
@@ -10,25 +10,26 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
-class TestHomeWork():
-    def setup_method(self, method):
-        self.driver = webdriver.Chrome()
-        self.vars = {}
-
-    def teardown_method(self, method):
-        self.driver.quit()
-
-    def test_homeWork(self):
-        self.driver.get("https://www.mazda.com/")
-        self.driver.set_window_size(1536, 835)
-        self.driver.find_element(By.ID, "header-search").click()
-        self.driver.find_element(By.ID, "header-search").send_keys("Mazda 2")
-        self.driver.find_element(By.CSS_SELECTOR, ".button-search").click()
-        assert self.driver.find_element(By.CSS_SELECTOR, ".result_txt > p").text == "Results for \"Mazda 2\" , 510 Results Found."
-        assert self.driver.title == "MAZDA:Search Result"
-        first_result = self.driver.find_element(By.CSS_SELECTOR, "#contents_2014 > div.result_detail_area > div:nth-child(1) > h3 > a)")
-        print("First search result:", first_result.text)
-
+class Test_Mazda():
+  def setup_method(self, method):
+    self.driver = webdriver.Chrome()
+    self.vars = {}
+  
+  def teardown_method(self, method):
+    self.driver.quit()
+  
+  def test_adfgasdfsadfas(self):
+    self.driver.get("https://www.mazda.com/")
+    self.driver.set_window_size(1536, 835)
+    self.driver.find_element(By.ID, "header-search").click()
+    self.driver.find_element(By.ID, "header-search").send_keys("Mazda 2")
+    self.driver.find_element(By.CSS_SELECTOR, ".button-search").click()
+    assert self.driver.find_element(By.CSS_SELECTOR, ".result_txt > p").text == "Results for \"Mazda 2\" , 510 Results Found."
+    assert self.driver.title == "MAZDA:Search Result"
+    first_result = self.driver.find_element(By.CSS_SELECTOR, ".detail_cont:nth-child(1) > h3 > a")
+    print("Перший знайдений результат:", first_result.text)
 if __name__ == "__main__":
-    pytest.main([__file__, '-s'])
-
+  test_intance = Test_Mazda()
+  test_intance.setup_method(None)
+  test_intance.test_adfgasdfsadfas()
+  test_intance.teardown_method(None)
